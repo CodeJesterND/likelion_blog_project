e3951132c75caa219a9e231cdead55b4130f0f47
CodeJesterND <dev.hyoseung@gmail.com>
2024-07-10 19:23:37 +0900

    🚜 Remove: 프로젝트 구조 변경

--- src/main/java/com/techit/global/auth/jwt/service/TokenRenewalService.java
+++
@@ -1,7 +0,0 @@
-package com.techit.global.auth.jwt.service;
-
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface TokenRenewalService {
-    public String reissueAccessToken(HttpServletResponse response, String refreshToken);
-}
